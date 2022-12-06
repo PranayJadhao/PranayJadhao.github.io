a4f7017c28b9636a64b6b601751d1fcdeac98ba8
PranayJadhao <pranayjadhao95@gmail.com>
2022-12-06 13:40:25 +0530

    adding

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Fabars,FaTimes} from "react-icons/fa"
+import styled from  "styled-components"
+
+
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-red fixed '>
+        <div>
+            <h1>Pranay</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
+
+
+
+

1c31f03e5fdb90541d26a7f7e5482c90e01c9cf1
PranayJadhao <pranayjadhao95@gmail.com>
2023-02-28 13:17:08 +0530

    adding

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,19 +17,21 @@ const Projects = () => {
         {
             id:1,
             src:modensenseProject,
+            title:"Modensense Clone",
             demo:'https://drive.google.com/file/d/1c4KmAwfK2ZSuC5LCCrAKIf2zhJO9zwra/view?usp=sharing',
             site:'https://agile-wheel-3721-l8yj.vercel.app/',
             code:'https://github.com/PranayJadhao/agile-wheel-3721',
-            description: "It is an e-commerse website which sales fashion product online.",
+            description: "Modensense is a multicategory online shopping platform, that currently offers a wide range of party wear and Daily wear Fashion.",
             tech:[<AiFillHtml5 size={25}/>,<DiCss3 size={25}/>,<IoLogoJavascript size={25}/>,<FaReact size={25}/>,<SiChakraui size={25}/>],
             type:'It is a solo project.'
         },
         {
             id:2,
             src:myntraProject,
+            title:"Myntra Clone",
             demo:'https://drive.google.com/file/d/14G6cM2X9IpuGlqlAMx3aFR15KZT-Nfp6/view',
             code:"https://github.com/PranayJadhao/sensitive-government-8954",
-            description: "It is an e-commerse website which sales product online.",
+            description: "Myntra is an e-commerse website which offers a mens and womens fashion wear.",
             tech:[<AiFillHtml5 size={25}/>,<DiCss3 size={25}/>,<IoLogoJavascript size={25}/>],
             site:'https://lovely-nasturtium-f92967.netlify.app/',
             type:'It is a solo project.'
@@ -37,9 +39,10 @@ const Projects = () => {
         {
             id:3,
             src:unsplashProject,
+            title:"Unsplash Clone",
             demo:"https://drive.google.com/file/d/1R191DWOgMGLTq7Gx0J7kwqUf301w_8Ju/view?usp=sharing",
             code:"https://github.com/PranayJadhao/exclusive-oatmeal-9601",
-           description:'From this website you can download images of your choice.',
+           description:'Unsplash provide you wide range of wallpapers,images of your choice for your phone,tablete,laptop etc',
             tech:[<AiFillHtml5 size={25}/>,<DiCss3 size={25}/>,<IoLogoJavascript size={25}/>,<FaReact size={25}/>,<SiChakraui size={25}/>],
             site:'https://exclusive-oatmeal-9601-my-project-rct-101-hxvtpj.vercel.app/',
             type:'It is a solo project.'
@@ -48,9 +51,10 @@ const Projects = () => {
         {
             id:4,
             src:tripasvisorProject,
+            title:"Tripadvisor Clone",
             demo:"https://drive.google.com/file/d/13Xd4dayrCQHfhMJc44u7oCRchVK3pD58/view?usp=sharing",
             code:"https://github.com/yogeshkumawat007/truculent-trains-1017",
-            description:"This website help you to book hotels online.",
+            description:"Tripadvisior helps you to travel in our country by helping to book hotels, restaurents, etc.",
             tech:[<AiFillHtml5 size={25}/>,<DiCss3 size={25}/>,<IoLogoJavascript size={25}/>],
             site:'https://splendorous-hotteok-40a851.netlify.app/',
             type:'It is a group project done with 4 members.'
@@ -66,14 +70,17 @@ const Projects = () => {
             <p className='py-6'>Check out some of my projects here</p>
         </div>
 
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-12 sm:px-0'>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 px-12 sm:px-0'>
             {
-                projectsArray.map(({id,src,demo,code,description,tech,type,site})=>(
+                projectsArray.map(({id,src,demo,code,description,tech,type,site,title})=>(
                     <div key={id} className='shadow-md shadow-zinc-600 rounded-lg  bg-zinc-600'>
                     <a href={site} target="_blank">
                     <img className='rounded-md duration-200 hover:scale-105'
                      src={src}/>
                     </a>
+                    <h2 className='justify-centre  items-center p-3 font-bold text-orange-500 text-lg'>
+                        {title} 
+                    </h2>
                     <div className='justify-centre  items-center p-3 '>
                         {description} 
                     </div>
